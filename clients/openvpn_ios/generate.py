13caa55b7c53802d3543f9e188c2ae80544d6b6c
Ivor Wanders <ivor@iwanders.net>
2018-01-13 12:17:15 -0500

    Config changes to accomodate OpenVPN Connect 1.2.5

--- clients/openvpn_ios/generate.py
+++ clients/openvpn_ios/generate.py
@@ -48,9 +48,6 @@ if __name__ == "__main__":
                         help='use tcp or udp')
     parser.add_argument("--port", type=int, default=443,
                         help='the port to use')
-    parser.add_argument("--password", type=str,
-                        default=None,
-                        help='The password for the pk12 file to be loaded.')
 
     parser.add_argument('--UserDefinedName', type=str, default=None,
                         help='User defined name, as shown in VPN menu'
@@ -101,23 +98,14 @@ if __name__ == "__main__":
             'with vpn on demand'.format(host=args.host, port=args.port,
                                         proto=args.proto)
 
-    if not args.password:
-        args.password = input("Type the .p12 file password:")
-
-    """
-    iOS mandates that the p12 file does NOT include the ca, which is default
-    from easyrsa3.
-    That could be done with:
-    $ openssl pkcs12 -export -out client_pkcs.p12 -inkey client.key \
-        -in client.crt  -name  $(basename $(pwd))
-    Testing appears that it is no problem if the p12 file also contains the ca.
-    """
-    with open("client_pkcs.p12", "rb") as f:
-        # requires to be encoded in base 64
-        p12_b64 = base64.b64encode(f.read()).decode('utf-8')
-
     with open("ca.crt", "r") as f:
         ca_crt = f.read()
+
+    with open("client.crt", "r") as f:
+        client_crt = f.read()
+
+    with open("client.key", "r") as f:
+        client_key = f.read()
 
     with open("on_demand.mobileconfig.xml", "r") as f:
         template = f.read()
@@ -132,13 +120,14 @@ if __name__ == "__main__":
         return "\n".join([line[i:i+w] for i in range(0, len(line), w)])
 
     res = template.format(
-            host=args.host, ca_crt=ca_crt.replace("\n", "\\n"),
-            internal_key_and_cert=wrap(p12_b64),
+            host=args.host,
+            ca_crt=ca_crt.replace("\n", "\\n"),
+            client_crt=client_crt.replace("\n", "\\n"),
+            client_key=client_key.replace("\n", "\\n"),
             pkcs_payload_uuid=str(uuid.uuid4()).upper(),
             vpn_payload_uuid=str(uuid.uuid4()).upper(),
             proto=args.proto,
             port=args.port,
-            password=args.password,
             config_payload_uuid=str(uuid.uuid4()).upper(),
             UserDefinedName=args.UserDefinedName,
             ProfilePayloadOrganization=args.ProfilePayloadOrganization,
