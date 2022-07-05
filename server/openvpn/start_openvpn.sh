f4eed2eb8654305de82e1c4b663d2fe3dbc0b343
Ivor Wanders <ivor@iwanders.net>
2022-07-04 21:00:46 -0400

    Fix client-to-client vpn communication.

--- server/openvpn/start_openvpn.sh
+++ server/openvpn/start_openvpn.sh
@@ -2,9 +2,10 @@
 
 VPNLAN='172.30.2'
 
-# we don't expose vpnhttps anymore, we use sslh to deal with the exposing.
-docker run -d -P --restart always --privileged --cap-add=NET_ADMIN --ip $VPNLAN.3 --net vpnlan --name vpnhttps openvpn:latest ovpn_run --https
-docker run -d -P --restart always --privileged --cap-add=NET_ADMIN --ip $VPNLAN.4 --net vpnlan --name vpnudp   --expose=1194/udp -p 1194:1194/udp openvpn:latest ovpn_run --udp
+# We don't expose vpnhttps anymore, we use sslh to deal with the exposing. Ip is passed in as a second
+# argument to allow the route for eth0 to be changed easily.
+docker run -d -P --restart always --privileged --cap-add=NET_ADMIN --ip $VPNLAN.3 --net vpnlan --name vpnhttps openvpn:latest ovpn_run --https $VPNLAN.3
+docker run -d -P --restart always --privileged --cap-add=NET_ADMIN --ip $VPNLAN.4 --net vpnlan --name vpnudp   --expose=1194/udp -p 1194:1194/udp openvpn:latest ovpn_run --udp  $VPNLAN.4
 
 
 
